d0878da84fcc2330dad651141fff38be4216bca3
jeff <jeff.silence@gmail.com>
2022-05-14 22:08:15 +0200

    Updated score

--- src/GuessingGame/GuessingGame.java
+++ src/GuessingGame/GuessingGame.java
@@ -11,6 +11,7 @@ public class GuessingGame {
         System.out.println("The correct number is: " + computerNumber);
         // Intial try count set at 10, will be deducted with each attempt
         int count = 10;
+        int score = 100;
 
         // Loop for game. After each try, the count variable is deducted by 1.
         // After the try counter hits 0, the game ends with a break.
@@ -19,14 +20,14 @@ public class GuessingGame {
             String response = JOptionPane.showInputDialog(null,
                     "Guess a number between 1 and 100", "Guessing Game", 3);
             userAnswer = Integer.parseInt(response);
-            JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, count));
+            JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, count, score));
             count--;
             if(count==-1)
                 break;
         }
     }
 
-    public static String determineGuess(int userAnswer, int computerNumber, int count){
+    public static String determineGuess(int userAnswer, int computerNumber, int count, int score){
         //checks if the try counter reached 0
         if(count == 0) {
             return "Sorry. You didn't get the right number.\nThe correct number is: " + computerNumber;
@@ -35,18 +36,16 @@ public class GuessingGame {
         if (userAnswer <=0 || userAnswer >100) {
             return "Uh, do over.\nGuess a number between 1 and 10";
         }
-        else if (userAnswer == computerNumber ){
-            return "Correct!\nTotal Guesses: " + count;
+        if (userAnswer == computerNumber ){
+            return "Correct!\nTotal Guesses: " + count + "\nYou're score: " + (count * score);
         }
-        else if (userAnswer > computerNumber) {
+        if (userAnswer > computerNumber) {
             return "Woops. Too high. Guess again.\nTries left: " + count;
         }
-        else if (userAnswer < computerNumber) {
+        if (userAnswer < computerNumber) {
             return "Not quite. Too low. Guess again.\nTries left: " + count;
         }
-        else {
-            return "Sorry. You didn't get the right number.\nThe correct number is: " + computerNumber;
-        }
+        return null;
     }
 }
 
